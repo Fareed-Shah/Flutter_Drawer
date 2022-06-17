10bd263be69615ab31d768daf41ec1997acb31ca
Fareed-Shah <syedfareed667@gmail.com>
2022-06-18 00:18:55 +0500

    bottomnavigationbar

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -5,12 +5,12 @@ import 'package:flutter_drawer/drawer_widget.dart';
 
 class Drawer_Screen extends StatefulWidget {
   const Drawer_Screen({Key? key}) : super(key: key);
-
   @override
   State<Drawer_Screen> createState() => _Drawer_ScreenState();
 }
 
 class _Drawer_ScreenState extends State<Drawer_Screen> {
+  int currentindex = 0;
   @override
   Widget build(BuildContext context) {
   return Scaffold(
@@ -21,26 +21,37 @@ class _Drawer_ScreenState extends State<Drawer_Screen> {
         centerTitle: true,
     ),
     
-    bottomNavigationBar:
-    BottomNavigationBar(
-      currentIndex: 1,
-      selectedFontSize: 17.0,
-      // selectedItemColor: Colors.red,
-      iconSize: 25.0,
-      // unselectedFontSize: 20.0,
+    bottomNavigationBar: BottomNavigationBar(
 
+        currentIndex: currentindex, 
+        onTap: (index) => setState(() {currentindex = index;}),
+        selectedFontSize: 17.0,      
+        iconSize: 25.0,      
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        type: BottomNavigationBarType.fixed,
       items:
-    [
-      BottomNavigationBarItem(
-        icon: Icon(Icons.home),
-        backgroundColor: Colors.red,
-        label: 'Home'),
-        
-        BottomNavigationBarItem(
-        icon: Icon(Icons.feed),
-        backgroundColor: Colors.blue,
-        label: 'Feed'),
-    ]
+        [
+             BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              backgroundColor: Colors.red,        
+              label: 'Home'),
+              
+             BottomNavigationBarItem(
+              icon: Icon(Icons.contact_page),
+              backgroundColor: Colors.red,        
+              label: 'Contact'),
+
+              BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              backgroundColor: Colors.red,        
+              label: 'Setting'),
+
+              BottomNavigationBarItem(
+              icon: Icon(Icons.history),
+              backgroundColor: Colors.red,        
+              label: 'History'),
+        ]
     ),
     
     drawer: My_Drawer(),
